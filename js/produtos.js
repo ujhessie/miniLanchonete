16c57a7298a61f934a09ea99eb3b50dda36872be
ujhessie <juniorrodrigues4502@gmail.com>
2023-04-29 22:26:07 -0300

    a

--- js/produtos.js
+++ js/produtos.js
@@ -1,65 +1,65 @@
-const produtos = [
+// const produtos = [
 
-    {id: 1, nome: 'Hamburger Vegetariano', valor: 20.00, desc: 'Um maravilhoso hamburguer vegetariano.Bom para toda a família'},
-    {id: 2, nome: 'Macarronada', valor: 30.00, desc: 'Uma macarronada completa e cheia de sabor'},
-    {id: 3, nome: 'Cachorro-Quente', valor: 10.00, desc: 'Sim. Um cachorro quenete. Maravilhoso. Não?'},
-    {id: 4, nome: 'Mistos Diversos', valor: 5.00, desc: 'Aqui eu fiquei com preguiça de arrumar'},
-    {id: 4, nome: 'Mistos Diversos', valor: 5.00, desc: 'Aqui eu fiquei com preguiça de arrumar'},
-    {id: 4, nome: 'Mistos Diversos', valor: 5.00, desc: 'Aqui eu fiquei com preguiça de arrumar'},
-    {id: 4, nome: 'Mistos Diversos', valor: 5.00, desc: 'Aqui eu fiquei com preguiça de arrumar'},
-    {id: 4, nome: 'Mistos Diversos', valor: 5.00, desc: 'Aqui eu fiquei com preguiça de arrumar'}
-]
-
-
-const divProdutos = document.querySelector('.div-produtos')
+//     {id: 1, nome: 'Hamburger Vegetariano', valor: 20.00, desc: 'Um maravilhoso hamburguer vegetariano.Bom para toda a família'},
+//     {id: 2, nome: 'Macarronada', valor: 30.00, desc: 'Uma macarronada completa e cheia de sabor'},
+//     {id: 3, nome: 'Cachorro-Quente', valor: 10.00, desc: 'Sim. Um cachorro quenete. Maravilhoso. Não?'},
+//     {id: 4, nome: 'Mistos Diversos', valor: 5.00, desc: 'Aqui eu fiquei com preguiça de arrumar'},
+//     {id: 4, nome: 'Mistos Diversos', valor: 5.00, desc: 'Aqui eu fiquei com preguiça de arrumar'},
+//     {id: 4, nome: 'Mistos Diversos', valor: 5.00, desc: 'Aqui eu fiquei com preguiça de arrumar'},
+//     {id: 4, nome: 'Mistos Diversos', valor: 5.00, desc: 'Aqui eu fiquei com preguiça de arrumar'},
+//     {id: 4, nome: 'Mistos Diversos', valor: 5.00, desc: 'Aqui eu fiquei com preguiça de arrumar'}
+// ]
 
 
-function renderizarProdutos(id, nome, valor, desc) {
-
-    // criando divProduto
-    const divProduto = document.createElement('div')
-    divProduto.setAttribute('class', 'div-produto')
-
-    // Criando elemento 'div-img-produto'
-    const divImgProduto = document.createElement('div')
-    divImgProduto.setAttribute('class', 'div-img-produto')
-
-    // criando o elemnto 'img-produto'
-    const imgProduto = document.createElement('img')
-    imgProduto.setAttribute('class', 'img-produto')
-    imgProduto.setAttribute('src', `img/produtos/${id}.png`)
-
-    // criando o elemento 'nome-produto'
-    const nomeProduto = document.createElement('h3')
-    nomeProduto.setAttribute('class', 'nome-produto')
-    nomeProduto.innerHTML = `${nome}`
-
-    // criando elemento 'valor-produto'
-    const valorProduto = document.createElement('h4')
-    valorProduto.setAttribute('class', 'valor-produto')
-    valorProduto.innerHTML = `R$ ${valor},00`
+// const divProdutos = document.querySelector('.div-produtos')
 
 
-    // criando elemento 'desc-produto'
-    const descProduto = document.createElement('p')
-    descProduto.setAttribute('class', 'desc-produto')
-    descProduto.innerHTML = `${desc}`
+// function renderizarProdutos(id, nome, valor, desc) {
+
+//     // criando divProduto
+//     const divProduto = document.createElement('div')
+//     divProduto.setAttribute('class', 'div-produto')
+
+//     // Criando elemento 'div-img-produto'
+//     const divImgProduto = document.createElement('div')
+//     divImgProduto.setAttribute('class', 'div-img-produto')
+
+//     // criando o elemnto 'img-produto'
+//     const imgProduto = document.createElement('img')
+//     imgProduto.setAttribute('class', 'img-produto')
+//     imgProduto.setAttribute('src', `img/produtos/${id}.png`)
+
+//     // criando o elemento 'nome-produto'
+//     const nomeProduto = document.createElement('h3')
+//     nomeProduto.setAttribute('class', 'nome-produto')
+//     nomeProduto.innerHTML = `${nome}`
+
+//     // criando elemento 'valor-produto'
+//     const valorProduto = document.createElement('h4')
+//     valorProduto.setAttribute('class', 'valor-produto')
+//     valorProduto.innerHTML = `R$ ${valor},00`
 
 
-    // Adicionando filhos aos pais
-    divImgProduto.appendChild(imgProduto)
-    divProduto.appendChild(divImgProduto)
-    divProduto.appendChild(nomeProduto)
-    divProduto.appendChild(valorProduto)
-    divProduto.appendChild(descProduto)
-
-    divProdutos.appendChild(divProduto)
+//     // criando elemento 'desc-produto'
+//     const descProduto = document.createElement('p')
+//     descProduto.setAttribute('class', 'desc-produto')
+//     descProduto.innerHTML = `${desc}`
 
 
-}
+//     // Adicionando filhos aos pais
+//     divImgProduto.appendChild(imgProduto)
+//     divProduto.appendChild(divImgProduto)
+//     divProduto.appendChild(nomeProduto)
+//     divProduto.appendChild(valorProduto)
+//     divProduto.appendChild(descProduto)
 
-for (let i = 0; i < produtos.length; i++) {
-    renderizarProdutos(produtos[i].id, produtos[i].nome, produtos[i].valor, produtos[i].desc)
-}
+//     divProdutos.appendChild(divProduto)
 
-// renderizarProdutos(001, 'hamburguer', 20.00, 'isto é apenas um teste')    
+
+// }
+
+// for (let i = 0; i < produtos.length; i++) {
+//     renderizarProdutos(produtos[i].id, produtos[i].nome, produtos[i].valor, produtos[i].desc)
+// }
+
+// // renderizarProdutos(001, 'hamburguer', 20.00, 'isto é apenas um teste')    
