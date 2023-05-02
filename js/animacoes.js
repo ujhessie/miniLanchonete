c510d953a7744da1bba25a33ee20fabcc8e94bdc
ujhessie <juniorrodrigues4502@gmail.com>
2023-05-02 15:18:29 -0300

    A

--- js/animacoes.js
+++ js/animacoes.js
@@ -1,12 +1,15 @@
 const btnMostrarCarrinho = document.querySelector('#btn-mostrar-carrinho')
 const btnFecharCarrinho = document.querySelector('#btn-fechar-carrinho')
 const janelaCarrinho = document.querySelector('.janela-carrinho')
+const main = document.querySelector('main')
 
 function mostrarCarrinho() {
     console.log('mostrando carrinho')
     janelaCarrinho.classList.add('janela-carrinho-ativada')
+    main.classList.add('sem-foco')
 }
 
 function fecharCarrinho() {
     janelaCarrinho.classList.remove('janela-carrinho-ativada')
+    main.classList.remove('sem-foco')
 }
